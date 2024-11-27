31faf2a2b497b79001dd14dccac47aa48dc12c0c
Julius Lange <julius@jlange.eu>
2024-11-27 11:32:35 +0100

    Initial starting point test project

--- resources/views/about.blade.php
+++
@@ -1,2 +0,0 @@
-<h1>Hello World, this is a Laravel test project</h1>
-<p>Magna ipsum nisi ut ipsum non. Duis laboris exercitation excepteur adipisicing. Dolor eu tempor sit non dolor. Sint officia elit laborum enim amet incididunt reprehenderit ad laboris. Qui aliqua voluptate adipisicing proident minim.</p>
--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,19 @@
 <?php
 
-use App\Http\Controllers\CalculatorController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use function Pest\Laravel\get;
 
-Route::get('/', function () {
-    $aboutPageUrl = route('about');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-    return view('welcome');
-})->name('home');
 
-/*
-Named View Routes
-*/
-Route::view('/about-us', 'about') -> name('about');
 
-Route::controller(CalculatorController::class)->group(function () {
-    Route::get('/calculate/sum/{num1}+{num2}', 'addition')->name('calculate.sum')->whereNumber(['num1', 'num2']);
-    Route::get('/calculate/subtract/{num1}-{num2}', 'subtraction')->name('calculate.subtract')->whereNumber(['num1', 'num2']);
-});
+
+
 
 /**
  * Fallback Routes
  */
-
 Route::fallback(function () {
     return to_route('home');
 });
