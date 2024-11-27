d2546cc543a52d5a27d25d066173455752ce9ba7
Julius Lange <julius@jlange.eu>
2024-11-27 11:54:24 +0100

    Add new Blade template with variables

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class HomeController extends Controller
 {
     public function index() {
-        return "Home";
-//        return view('home');
+        return View::make('home.index')->with('title', 'Home')->with('subtitle', 'Hello World!!!!!');
     }
 }
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,4 @@
+<h1>Second: Hello World</h1>
+<h2>This is a laravel test project</h2>
+<h3>Current year: {{$year}}</h3>
+<p>You are on Page {{$title}} : {{$subtitle}}</p>
