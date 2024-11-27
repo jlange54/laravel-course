4176b567bfdd9bec7f0ec98edcdd5fbd817092d8
Julius Lange <julius@jlange.eu>
2024-11-27 14:12:51 +0100

    Directives -> foreach & $loop

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,10 +1,11 @@
-<script>
-    const hobbies = {{\Illuminate\Support\Js::from($hobbies)}}
-</script>
+<h1>Hello World</h1>
+{{--
+    Comment
+--}}
 
-
-<h1>Second: Hello World</h1>
-<h2>This is a laravel test project</h2>
-<h3>Current year: {{ date('Y') }}</h3>
-<p>You are on Page {{$title}} : {{$subtitle}}</p>
-<p>Job: {!! $job !!}</p>
+<p>Hobbies:</p>
+<ul>
+    @foreach($hobbies as $hobbie)
+        <li> {{ $loop->iteration }} {{$hobbie}}</li>
+    @endforeach
+</ul>
