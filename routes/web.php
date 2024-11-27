f91af61c4759a8a77043a366848f35ba4d9ad537
Julius Lange <julius@jlange.eu>
2024-11-27 08:51:39 +0100

    Add route required & optional parameters examples. Add comments

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*
+View Routes
+*/
 Route::view('/about', 'about');
+
+/*
+Route Required Parameters
+*/
+Route::get('/event/{id}', function ($id) {
+    return "Event id: $id";
+});
+
+/*
+Route Optional Parameters
+*/
+Route::get('/events/{category?}', function (string $category = null) {
+    return "Category: $category";
+});
