233cbbd2c1206169139baeffb4f5933848571b87
Julius Lange <julius@jlange.eu>
2024-11-27 10:23:17 +0100

    Add Singe Action Controller ShowCarController
    execute SAC from Route::get

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\CarController;
+use App\Http\Controllers\ShowCarController;
 use Illuminate\Support\Facades\Route;
+use function Pest\Laravel\get;
 
 Route::get('/', function () {
     $aboutPageUrl = route('about');
@@ -15,12 +17,14 @@ Named View Routes
 Route::view('/about-us', 'about') -> name('about');
 
 /**
- * Controller Groups
+ * Single Action Contollers
  */
-Route::controller(CarController::class)->group(function () {
-    Route::get('/cars', 'index') -> name('cars.index');
-    Route::get('/cars/create', 'create') -> name('cars.create');
-});
+//Route::controller(CarController::class)->group(function () {
+//    Route::get('/cars', 'index') -> name('cars.index');
+//    Route::get('/cars/create', 'create') -> name('cars.create');
+//});
+
+Route::get('/car',ShowCarController::class);
 
 /**
  * Fallback Routes
