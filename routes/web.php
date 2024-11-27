58a0a4b495ed469ec4bba938e8d156e37f95713d
Julius Lange <julius@jlange.eu>
2024-11-27 09:11:44 +0100

    Add route parameter validation exercises

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ Route::view('/about', 'about');
 /*
 Route Required Parameters
 */
-Route::get('/event/{id}', function ($id) {
-    return "Event id: $id";
-});
+//Route::get('/event/{id}', function ($id) {
+//    return "It works: $id";
+//});
 
 /*
 Route Optional Parameters
@@ -24,3 +24,19 @@ Route Optional Parameters
 Route::get('/events/{category?}', function (string $category = null) {
     return "Category: $category";
 });
+
+/**
+ * Route Parameter Validation
+ */
+
+Route::get('/user/{username}', function (string $username) {
+    return "User name: $username";
+})->where(['username' => '[a-z]+']);
+
+Route::get('{lang}/event/{id}', function ($lang, $id) {
+    return "It works: $id";
+})->where(['lang' =>'[a-z]{2}','id' => '\d{1,2}']);
+
+Route::get('/search/{keyword}', function (string $keyword) {
+    return "Search: $keyword";
+})->where(['keyword' => '.+']);
