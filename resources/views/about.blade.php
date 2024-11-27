85ea2ccd0a871cfeb7245e7c818741e33f1f8961
Julius Lange <julius@jlange.eu>
2024-11-27 08:33:32 +0100

    Added new route /about
    Routing exercise

---
+++ resources/views/about.blade.php
@@ -0,0 +1,2 @@
+<h1>Hello World, this is a Laravel test project</h1>
+<p>Magna ipsum nisi ut ipsum non. Duis laboris exercitation excepteur adipisicing. Dolor eu tempor sit non dolor. Sint officia elit laborum enim amet incididunt reprehenderit ad laboris. Qui aliqua voluptate adipisicing proident minim.</p>
