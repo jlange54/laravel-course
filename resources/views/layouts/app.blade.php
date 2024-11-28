8b14911065b1d9a042a844e8e54ac0a27ba2ddc5
Julius Lange <julius@jlange.eu>
2024-11-28 09:36:13 +0100

    More directives: @hasSection

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,10 @@
     @include('layouts.partials.header')
     @yield('content')
 
-    <footer>
-        @section('footerLinks')
-            <a>Link 1</a>
-            <a>Link 2</a>
-        @show
-    </footer>
+    @hasSection('footerLinks')
+        <footer>
+           @yield('footerLinks')
+        </footer>
+    @endif
+
 @endsection
